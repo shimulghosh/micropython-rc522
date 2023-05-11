d2a9e168b02c3712e265aba2db2db93f386287a4
shimulghosh <shimulghosh18@gmail.com>
2023-05-11 20:35:39 +0200

    commit

--- paste_to_ntfy.py
+++ paste_to_ntfy.py
@@ -2,13 +2,14 @@ import requests
 import network
 url_users='https://pastebin.com/raw/BDCw0mWn'
 url_ntfy='https://ntfy.sh/FabLabGuest'
-users={}
+users=[]
 try:
   res = requests.get(url_users)
   for user in (res.text).split('\n'):
-    users[user.split(':')[0]]=user.replace('\r','').split(':')[1]
-  print(users)
+    user=user.replace('\r','')
+    user=user.split(':')[1]
+    users.append(user)
 except:
   pass
-
+print(users)
 res = requests.post(url_ntfy, data = users)
